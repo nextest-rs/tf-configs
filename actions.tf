f458797ee990b404b55bab9538cdc08e8992bc84
Rain <rain@sunshowers.io>
2026-07-25 16:41:25 -0700

    add actions config

---
+++ actions.tf
@@ -0,0 +1,30 @@
+resource "github_actions_organization_permissions" "this" {
+  enabled_repositories = "all"
+  allowed_actions      = "selected"
+
+  allowed_actions_config {
+    github_owned_allowed = true
+    verified_allowed     = true
+
+    # Enumerated from actions actually used across org workflows.
+    # Add new third-party actions here.
+    patterns_allowed = [
+      "JamesIves/github-pages-deploy-action@*",
+      "Swatinem/rust-cache@*",
+      "actions-rs/*",
+      "cloudflare/*",
+      "codecov/*",
+      "dtolnay/rust-toolchain@*",
+      "eifinger/setup-uv@*",
+      "michidk/run-komac@*",
+      "model-checking/kani-github-action@*",
+      "opentofu/setup-opentofu@*",
+      "renovatebot/github-action@*",
+      "rust-lang/crates-io-auth-action@*",
+      "signpath/github-action-submit-signing-request@*",
+      "taiki-e/*",
+      "trunk-io/analytics-uploader@*",
+      "zizmorcore/zizmor-action@*",
+    ]
+  }
+}
